382d19cd18725f8de5bd39ccce49e3fc3a90f80f
Merwan Laouini <merwan@MacBook-Pro-de-Merwan.local>
2021-10-05 15:33:52 +0200

    Exo todo list singl page vue JS

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <TodoList/>
+    <TodoListItemDone/>
+    <TodoListItem/>
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+import TodoListItemDone from './components/TodoListItemDone.vue';
+import TodoListItem from './components/TodoListItem.vue';
+
+export default {
+  name: "App ,",
+  components: {
+    TodoList,
+    TodoListItemDone,
+    TodoListItem,
+    
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todolist" id="app">
+    <ol>
+      <li v-for="(item, index) in list" :key="item.message">
+        <todo-list-item :newTask="newTask" :list="list" :item="item" :index="index"></todo-list-item>
+      </li>
+    </ol>
+    <form action="" @submit.prevent="addTask">
+      <input type="text" v-model="newTask" />
+      <input type="submit" value="Ajouter une tâche" />
+    </form>
+  </div>
+</template>
+
+<script>
+import TodoListItem from './TodoListItem.vue';
+
+export default {
+  components: { TodoListItem },
+  data() {
+    return {
+      newTask: "",
+      list: [{
+        message: "Faire une liste de course",
+        done: false
+      }],
+    };
+  },
+  methods: {
+    addTask: function () {
+      this.list.push({
+        message: this.newTask,
+        done: false,
+      });
+      this.newTask = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
